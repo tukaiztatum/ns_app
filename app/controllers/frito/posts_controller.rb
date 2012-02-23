29e68191407a5f9ef7425d2cb4712a27043c13ad
Tatum Szymczak <t.szymczak@tukaiz.com>
2012-02-23 08:52:53 -0600

    wip

--- app/controllers/frito/posts_controller.rb
+++ app/controllers/frito/posts_controller.rb
@@ -1,5 +1,26 @@
 class Frito::PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.find(:all)
   end
+
+  def new
+    @post = Frito::Post.new
+  end
+
+  def create
+    @post = Frito::Post.new(params[:frito_post])
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
+  def show
+    @post = Frito::Post.find(params[:id])
+
+  end
+
 end
